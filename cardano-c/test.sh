e3d18fbcec4fb362cb5d165195eb7ede41637ba6
Dat Nguyen Van <datnv@blockchainlabs.asia>
2018-09-25 15:08:32 +0700

    Test transaction c bindings, not finished yet

--- cardano-c/test.sh
+++ cardano-c/test.sh
@@ -10,15 +10,15 @@ else
 fi
 
 
-C_ROOT="${PROJECT_ROOT}cardano-c/"
-C_LIB_A="${PROJECT_ROOT}target/debug/libcardano_c.a"
+C_ROOT="./"
+C_LIB_A="./dist/cardano-c/x86_64-unknown-linux-gnu/debug/libcardano_c.a"
 
 if [ ! -f "${C_LIB_A}" ]; then
 	echo "no library file found. compile cardano-c first"
 	exit 2
 fi
 
-gcc -o test-cardano-c.$$ -I "${C_ROOT}" "${C_ROOT}test/test.c" "${PROJECT_ROOT}target/debug/libcardano_c.a" -lpthread -lm -ldl
+gcc -o test-cardano-c.$$ -I "${C_ROOT}" "${C_ROOT}test/test.c" "${C_LIB_A}" -lpthread -lm -ldl
 echo "######################################################################"
 ./test-cardano-c.$$
 echo ""
--- cardano-c/test/test.c
+++ cardano-c/test/test.c
@@ -26,6 +26,28 @@ int wallet_test(void) {
 
 	cardano_wallet_delete(wallet);
 
+	// Transaction test
+	static const char* root_key = "d8a7234357dcfc003c99ac410262de9bf2b43c1886939045012d270de6cb2f4360453ef620552718dece81b9b0efcc55a71a5b9a417ccf772fe90ec857c2f0da77794c3cacfc998a9f2ad30f17b6370a9a56695aa35ea702e7abd430d7615637";
+	static const char* txid = "678f01893645b40557166a52637e6a5db048d34f09da6096a7ceb63abfeb5187";
+
+	cardano_xprv *xprv = cardano_xprv_from_bytes(root_key);
+	cardano_xpub *to_addr = cardano_xprv_to_xpub(xprv);
+	cardano_xpub *change_addr = cardano_xprv_to_xpub(xprv);
+
+	cardano_txoptr *txoptr = cardano_transaction_output_ptr_new(txid, 0);
+	if (!txoptr) goto error;
+
+	cardano_txoutput *txoutput = cardano_transaction_output_new(to_addr, 1000000);
+	if (!txoutput) goto error;
+
+	cardano_transaction_builder *builder = cardano_transaction_builder_new();
+	if (!builder) goto error;
+
+	cardano_transaction_builder_add_output(builder, txoutput);
+	cardano_transaction_builder_add_input(builder, txoptr, 600000);
+	cardano_transaction_builder_add_change_addr(builder, change_addr);
+
+
 	return 0;
 error:
 	return -1;
